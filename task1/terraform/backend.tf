568f60cbc6c2ba75d3023fca5fe87ecd5878f685
user <user@users-MacBook-Air.local>
2026-04-06 22:08:35 +0300

    fix: add spaces credentials to provider

--- task1/terraform/backend.tf
+++ task1/terraform/backend.tf
@@ -21,10 +21,12 @@ terraform {
     skip_metadata_api_check     = true
     skip_region_validation      = true
     skip_requesting_account_id  = true
-    force_path_style            = true
+    use_path_style              = true
   }
 }
 
 provider "digitalocean" {
-  token = var.do_token
+  token             = var.do_token
+  spaces_access_id  = var.spaces_access_key
+  spaces_secret_key = var.spaces_secret_key
 }
